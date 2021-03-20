60af3833826aa74bfa366e3eadb80ef268876168
Qkehan <q1079300361@qq.com>
2021-03-20 16:11:17 +0800

    Add files via upload

---
+++ mu-python/python/1.py
@@ -0,0 +1,7 @@
+# 在这里写上你的代码 :-)
+DaysPerYear=365
+HoursPerDay=24
+MinutesPerHour=60
+SecondsPerMinute=60
+SecondsPerYear=(DaysPerYear*HoursPerDay*MinutesPerHour*SecondsPerMinute)
+print(SecondsPerYear)
