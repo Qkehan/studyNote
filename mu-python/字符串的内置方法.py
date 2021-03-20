192b588020429c59f14d41a29bfeb1cdd6971dab
Qkehan <q1079300361@qq.com>
2021-03-20 08:16:26 +0000

    Delete 字符串的内置方法.py

--- mu-python/字符串的内置方法.py
+++
@@ -1,9 +0,0 @@
-# 在这里写上你的代码 :-)
-#字符串的各种内置方法。
-str = 'i love mylife'
-str1='change the world by progarm'
-
-#python中没有字符类型的，是以长度来定义的。
-
-#.capitalize()   #将字符串 句子第一个字母换成大写。
-#.casefold()     #将大写改为小写，将小写改为大写。
